56cc8aa3ea0fa04faad11a9623275bc527eeb6c6
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-11 17:21:08 -0500

    - Created a PriorityQueue class

---
+++ priority_queue.py
@@ -0,0 +1,33 @@
+import heapq
+"""
+Using heapq to build a priority queue as
+the heapq already has functionalities similar to a priority queue.
+"""
+
+class PriorityQueue:
+    """
+    PriorityQueue class will be used in Informed Search algorithms.
+    """
+
+    def __init__(self):
+        self.elements = []
+
+    def is_empty(self):
+        return not self.elements
+
+    def put(self, item, priority):
+        """
+        Pushing the tuple (priority, item) onto self.elements list.
+        """
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        """
+        Popping the element with the minimum priority value (i.e Highest priority).
+        We specify the index to be 1 because we are getting from the tuple (priority, item).
+        Therefore, we are popping item.
+        """
+        return heapq.heappop(self.elements)[1]
+
+    def __str__(self):
+        return str(self.elements)
