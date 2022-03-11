70a3cab5897f037837e9113446f7863cc38553bb
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-10 22:45:51 -0500

    small changes

--- driver.py
+++ driver.py
@@ -1,9 +1,9 @@
 from solver import Solver
 
 def main():
-    search_algorithm = 'dfs'
-    initial_state = [2, 8, 3, 1, 6, 4, 7, 0, 5]
-    goal_state = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+    search_algorithm = 'bfs'
+    initial_state = [2, 8, 3, 1, 6, 4, 7, "B", 5]
+    goal_state = [1, 2, 3, 8, "B", 4, 7, 6, 5]
 
     solver = Solver(initial_state, goal_state, search_algorithm)
     solver.solve()
