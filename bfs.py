6e68328ccad7708840b87fb701efa0f58b950d94
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-10 22:45:06 -0500

    Added BFS algorithm

---
+++ bfs.py
@@ -0,0 +1,29 @@
+import queue
+
+def BFS(initial_state):
+    open = queue.Queue()
+    open.put(initial_state)
+    # Used to check if the state is not already in the open list.
+    # This is for cases where the search makes a move that backtracks to the parent state
+    open_state = []
+    open_state.append(tuple(initial_state.currentState))
+    closed = set()
+    nodes_explored = 0
+    max_search_depth = 0
+
+    while open:
+        state = open.get()
+        closed.add(tuple(state.currentState))
+        if state.is_goal():
+            return (state, nodes_explored, max_search_depth)
+
+        nodes_explored = nodes_explored + 1
+
+        for child in state.expand():
+            if tuple(child.currentState) not in closed and tuple(child.currentState) not in open_state:
+                open.put(child)
+                open_state.append(tuple(child.currentState))
+                if child.cost > max_search_depth:
+                    max_search_depth = child.cost
+
+    return None
