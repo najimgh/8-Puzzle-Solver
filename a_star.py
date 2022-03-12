904be3c5ecb7758e05a9657a5e8b21a449f34167
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-12 15:54:11 -0500

    - Added comments

--- a_star.py
+++ a_star.py
@@ -1,10 +1,13 @@
 from priority_queue import PriorityQueue
 
 def A_STAR(initial_state, total_cost):
+    """
+    A* search algorithm
+    """
     # Using PriorityQueue defined in priority_queue.py
     open = PriorityQueue()
     open.put(initial_state, total_cost(initial_state))
-    # Used to check if the state is not already in the open list.
+    # open_state is used to check if the state is not already in the open list.
     # This is for cases where the search makes a move that backtracks to the parent state
     open_state = []
     open_state.append(tuple(initial_state.currentState))
@@ -15,12 +18,15 @@ def A_STAR(initial_state, total_cost):
     while not open.is_empty():
         state = open.get()
         closed.add(state)
+        # Check is state is the goal state
         if state.is_goal():
             return (state, nodes_explored, max_search_depth)
 
         nodes_explored = nodes_explored + 1
 
+        # Iterate over the children of the state
         for child in state.expand():
+            # If the child is not in the closed list or already in the open list
             if child not in closed and tuple(child.currentState) not in open_state:
                 open.put(child, total_cost(child))
                 open_state.append(tuple(child.currentState))
