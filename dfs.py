edf04b4a417be91cee56c6d028fcd88c7982b01a
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-12 15:59:48 -0500

    - Added comments

--- dfs.py
+++ dfs.py
@@ -1,6 +1,7 @@
 from collections import deque
 
 def DFS(initial_state):
+    # Open list is a deque which essentially behaves like a stack
     open = deque()
     open.append(initial_state)
     # Used to check if the state is not already in the open list.
@@ -14,12 +15,15 @@ def DFS(initial_state):
     while open:
         state = open.pop()
         closed.add(tuple(state.currentState))
+        # Check if state is the goal state
         if state.is_goal():
             return (state, nodes_explored, max_search_depth)
 
         nodes_explored = nodes_explored + 1
 
+        # Iterate over the children of the state
         for child in state.expand():
+            # If the child is not in the closed list or already in the open list
             if tuple(child.currentState) not in closed and tuple(child.currentState) not in open_state:
                 open.append(child)
                 open_state.append(tuple(child.currentState))
