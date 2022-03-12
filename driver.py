328eccd28407c205f3d4e00c9cd6223c67e1598a
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-11 19:55:11 -0500

    small changes

--- driver.py
+++ driver.py
@@ -1,11 +1,10 @@
 from solver import Solver
 
 def main():
-    search_algorithm = 'bfs'
     initial_state = [2, 8, 3, 1, 6, 4, 7, "B", 5]
     goal_state = [1, 2, 3, 8, "B", 4, 7, 6, 5]
 
-    solver = Solver(initial_state, goal_state, search_algorithm)
+    solver = Solver(initial_state, goal_state, 'astar', 'manhattan')
     solver.solve()
 
 if __name__ == '__main__':
