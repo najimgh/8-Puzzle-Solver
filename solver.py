eb84a0149e24e1b0b03ab25e6b16032c2fc2b8eb
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-12 16:23:49 -0500

    - Added comments

--- solver.py
+++ solver.py
@@ -8,6 +8,10 @@ from heuristics import hamming_distance, manhattan_distance, nilsson_sequence, p
 import time
 
 class Solver(object):
+    """
+    The Solver ckass is used to instatiate the object with the desired initial state, goal state, search algorithm and the heuristic (if applicable).
+    The Solver calculates the appropriate cost of a state and gives an output with different information when the 8-Puzzle is solved.
+    """
 
     def __init__(self, initial_state, goal, algorithm='bfs', heuristic= None):
         self.heuristic = heuristic
@@ -15,7 +19,7 @@ class Solver(object):
         self.initial_state = initial_state
         SIZE = 3
         """
-        When the heuristic is not defined, do not define the cost function
+        When a heuristic is not defined, do not define the cost function
         """
         if(heuristic is None):
             self.state = State(initial_state, SIZE, goal)
@@ -23,7 +27,7 @@ class Solver(object):
             self.state = State(initial_state, SIZE, goal, self.total_cost)
 
         """
-        Set the search algorithm function and heuristic function if it is applicable
+        Set the search algorithm function and heuristic function (if applicable)
         """
         if(algorithm == 'bfs'): 
             self.search_alg = BFS
@@ -56,8 +60,10 @@ class Solver(object):
         """
         sum_h = 0
         if(self.heuristic == 'nilsson'):
+             # h(n) comes from the Nilsson Sequence heuristic
             sum_h += self.distance(state)
         elif(self.heuristic == 'inversion'):
+            # h(n) comes from the Permutation Inversion heuristic
             sum_h = self.distance(state.currentState)
         else:
             for i, value in enumerate(state.currentState):
@@ -67,11 +73,12 @@ class Solver(object):
                 # Position of current value
                 current_row = i // state.size
                 current_col = i % state.size
-                # Position of goal  value
+                # Position of goal value
                 goal_value_index = state.goal.index(value)
                 goal_row = goal_value_index // state.size
                 goal_col = goal_value_index % state.size
         
+                # h(n) comes from either the hamming distance heuristic or the manhattan distance heuristic
                 sum_h += self.distance(current_row,current_col,goal_row,goal_col)
         # Return only the heuristic cost if the algorithm is GBFS 
         if(self.algorithm == 'gbfs'):
@@ -81,13 +88,25 @@ class Solver(object):
         return total_cost
 
     def output(self, result, r_time):
+        """
+        Prints to the console with information about the results obtained
+        depending the on the search algorithm and/or the heuristic used to solve the 8=Puzzle.
+        """
+        # Results from the search algorithm
         final_state, nodes_explored, max_search_depth = result
+        # The cost of the path to the goal state or g(n)
         path_cost = final_state.cost
+        # The actions taken to arrive to the goal state
         action_path = [final_state.operator]
+        # The states generated to arrive at the goal state
         state_path_to_goal = [final_state]
+        # The parent state
         parent_state = final_state.parent
         search_depth = 0
 
+        # Calculate the search path to the goal state
+        # Find the actions taken to arrive at the goal state
+        # Find the states generated to arrive at the goal state
         while parent_state:
             search_depth += 1
             action_path.append(parent_state.operator)
@@ -97,21 +116,22 @@ class Solver(object):
         state_path_to_goal.reverse()
 
         print("\n" + "------ Results ------" + "\n")
+
         print("action_path:", str(action_path) + "\n")
         print("State Path to Goal:" + "\n")
         for state in state_path_to_goal:
             state.display_state()
         print("\n")
-        if(self.algorithm == 'bfs' or self.algorithm == 'dfs'):
-            print("iteration count:", str(path_cost) + "\n")
-        else:
-            print("path_cost:", str(path_cost) + "\n")
+        print("path_cost:", str(path_cost) + "\n")
         print("nodes_explored:", str(nodes_explored) + "\n")
         print("search_depth:", str(search_depth) + "\n")
         print("max_search_depth:", str(max_search_depth) + "\n")
         print("running time:", str(r_time) + "\n")
 
     def solve(self):
+        """
+        Function used to start solving the puzzle
+        """
         start_time = time.time()
 
         if(self.search_alg == A_STAR or self.search_alg == GBFS):
