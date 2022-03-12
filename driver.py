a83cd27d33e9bcfb6a93f8539975d199fe041ff6
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-11 21:52:45 -0500

    small changes

--- driver.py
+++ driver.py
@@ -4,7 +4,7 @@ def main():
     initial_state = [2, 8, 3, 1, 6, 4, 7, "B", 5]
     goal_state = [1, 2, 3, 8, "B", 4, 7, 6, 5]
 
-    solver = Solver(initial_state, goal_state, 'astar', 'manhattan')
+    solver = Solver(initial_state, goal_state, 'gbfs', 'inversion')
     solver.solve()
 
 if __name__ == '__main__':
