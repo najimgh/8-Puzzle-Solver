a8287b51b70070d3cb012e42c5e751c87720d2ae
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-12 16:02:51 -0500

    - Added comments

--- gbfs.py
+++ gbfs.py
@@ -1,6 +1,10 @@
 from priority_queue import PriorityQueue
 
 def GBFS(initial_state, heuristic_cost):
+    """
+    Greedy Best-First search algorithm.
+    Using the heuristic cost to calculate the priority
+    """
     # Using PriorityQueue defined in priority_queue.py
     open = PriorityQueue()
     open.put(initial_state, heuristic_cost(initial_state))
@@ -15,12 +19,15 @@ def GBFS(initial_state, heuristic_cost):
     while not open.is_empty():
         state = open.get()
         closed.add(state)
+        # Check if state is the goal state
         if state.is_goal():
             return (state, nodes_explored, max_search_depth)
 
         nodes_explored = nodes_explored + 1
 
+        # Iterate over the children of the state
         for child in state.expand():
+            # If the child is not in the closed list or already in the open list
             if child not in closed and tuple(child.currentState) not in open_state:
                 open.put(child, heuristic_cost(child))
                 open_state.append(tuple(child.currentState))
