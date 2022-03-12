0d6beb342c8c4a0cf91d2c47ad6029c8b62f9e31
Najim Ghafourzadeh <najimg1@hotmail.com>
2022-03-12 15:46:47 -0500

    - Added comments

--- driver.py
+++ driver.py
@@ -4,7 +4,9 @@ def main():
     initial_state = [2, 8, 3, 1, 6, 4, 7, "B", 5]
     goal_state = [1, 2, 3, 8, "B", 4, 7, 6, 5]
 
-    solver = Solver(initial_state, goal_state, 'gbfs', 'inversion')
+    # Modify the solver based on the algorithms you want to run
+    solver = Solver(initial_state, goal_state, 'dfs')
+    # solver = Solver(initial_state, goal_state, 'astar', 'manhattan')
     solver.solve()
 
 if __name__ == '__main__':
